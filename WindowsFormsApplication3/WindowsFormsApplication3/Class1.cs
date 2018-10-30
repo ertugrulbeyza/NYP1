bee741078d9c87a11806a96eaeaa7c9d4579a508
ertugrulbeyza <44607768+ertugrulbeyza@users.noreply.github.com>
2018-10-30 15:04:58 +0300

    Add files via upload

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication3
+{
+    class Class1
+    {
+        string ad;
+
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        int yas;
+        public int Yas
+        {
+            get { return yas - 5; }
+            set { yas = value; }
+        }
+        int not1;
+        public int Not1
+        {
+            set { not1 = value; }
+        }
+        int not2;
+        public int Not2
+        {
+            set { not2 = value; }
+        }
+        int ort;
+        public int Ort
+        {
+            get {
+                ort = (not1 + not2) / 2;
+                return ort; }
+        }
+        public override string ToString()
+        {
+            return ad + "-" + yas + "ortalaması" + ort;
+        }
+    }
+}
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 ogr = new Class1();
+            ogr.Ad = textBox1.Text;
+            ogr.Yas = Convert.ToInt32(textBox1.Text);
+            ogr.Not1 = Convert.ToInt32(textBox2.Text);
+            //MessageBox.Show(ogr.Ort.ToString());
+            listBox1.Items.Add(ogr);
+
+        }
+    }
+}
